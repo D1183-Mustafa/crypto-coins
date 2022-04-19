f38426df376d318d344f6eb90f78bedeb260b6ee
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-04-19 04:32:02 +0300

    hesap componentinde işlemler yapıldı

--- src/components/sepet/Hesap.js
+++ src/components/sepet/Hesap.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import TextField from "@mui/material/TextField";
 import NativeSelect from "@mui/material/NativeSelect";
 import Button from "@mui/material/Button";
 import "./Hesap.css";
+import AppContext from "../../context/AppContext";
+
 function Hesap() {
+  const {newSepet} = useContext(AppContext);
   return (
     <div>
       <div className="hesap">
         <TextField
           id="standard-basic"
-          label="Search..."
+          label="..Miktar..(TRY)"
           variant="standard"
-          type="search"
+          type="number"
         />
         <NativeSelect
           defaultValue={30}
@@ -20,9 +23,13 @@ function Hesap() {
             id: "uncontrolled-native",
           }}
         >
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
+          {
+            newSepet.map((item,index) => {
+              return(
+                <option value={item.id} key={index}>{item.name}</option>
+              )
+            })
+          }
         </NativeSelect>
         <Button variant="contained" color="success">
           Satıl Al
